00ecbc9a429513206a7bbab120d7241e8c049309
didwogur28 <didwogur28@naver.com>
2023-06-13 23:16:55 +0900

    feat: pass-bach starter 구성
    PT 입장권 관리 프로젝트 초기 구성

---
+++ src/main/java/com/pass/repository/BookingStatus.java
@@ -0,0 +1,5 @@
+package com.pass.repository;
+
+public enum BookingStatus {
+    READY, IN_PROGRESS, COMPLETED, CANCELLED
+}
---
+++ src/main/java/com/pass/repository/user/UserStatus.java
@@ -0,0 +1,5 @@
+package com.pass.repository.user;
+
+public enum UserStatus {
+    ACTIVE, INACTIVE
+}
